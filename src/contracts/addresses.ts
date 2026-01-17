f42fa2c065390e34ac53c3b251782b3d0c9214c2
rosieluox2223-spec <rosieluox2223@gmail.com>
2026-01-17 19:23:55 +0800

    Update addresses.ts

--- src/contracts/addresses.ts
+++ src/contracts/addresses.ts
@@ -1,7 +1,7 @@
 // src/contracts/addresses.ts
 export const CONTRACT_ADDRESSES = {
   10143: {
-    KindToken: "0x你的KindToken地址",
-    KindFlowCore: "0x你的KindFlowCore地址",
+    KindToken: "0x3095530dEd371D63A36587d4ad674f655a6804F1",
+    KindFlowCore: "0x8C5E5C9d344Da9bE45070d261b858DC2A584303F",
   },
 } as const;
